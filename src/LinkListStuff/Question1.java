c68d94e7365bc2972a0d7d1ab8e4e6f911c37a40
ndw6152 <ndwong19@gmail.com>
2017-06-11 23:01:17 -0700

    Max sum of non contiguous digits in an array

--- src/LinkListStuff/Question1.java
+++ src/LinkListStuff/Question1.java
@@ -1,5 +1,8 @@
 package LinkListStuff;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * Created by Anonealio on 6/5/2017.
@@ -62,13 +65,52 @@ public class Question1 {
         System.out.println(result);
     }
 
+    private ArrayList<Integer> copyArr(ArrayList<Integer> current, ArrayList<Integer> result) {
+        result.addAll(current);
+        return result;
+    }
+
+    public void largestSumOfNonContiguousNumbers(ArrayList<Integer> arr) {
+
+        HashMap<Integer, ArrayList<Integer>> table = new HashMap<>();
+
+        ArrayList<Integer> maxArr = new ArrayList<>();
+        ArrayList<Integer> tempArr = new ArrayList<>();
+        int maxSum = 0;
+        int temp = 0;
+        for(int i = 0; i< arr.size(); i++) {
+            temp += arr.get(i);
+            tempArr.add(arr.get(i));
+            if(temp > maxSum) {
+                maxSum = temp;
+                maxArr.clear();
+                for(Integer val : tempArr) {
+                    maxArr.add(val);
+                }
+            }
+            if(temp < 0) {
+                temp = 0;
+                // reset map?
+                tempArr.clear();
+            }
+        }
+
+        System.out.println(maxSum);
+
+    }
+
+
+
+
     public static void main(String[] args) {
         System.out.println("Running question 1");
         Question1 program = new Question1();
-        //program.runReverse();
+        // program.runReverse();
+        // program.reverseString("Testing");
 
-
-        program.reverseString("Testing");
+        ArrayList<Integer> arr = new ArrayList();
+        arr.add(1); arr.add(-1); arr.add(50); arr.add(-100); arr.add(5); arr.add(-10); arr.add(15);
+        program.largestSumOfNonContiguousNumbers(arr);
 
     }
 }
