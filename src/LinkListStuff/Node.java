f054973fb1b2e5c6d05110fe00ccbd3bba4ae430
ndw6152 <ndwong19@gmail.com>
2017-08-21 18:03:25 -0700

    Added contructor for Node that does not take next

--- src/LinkListStuff/Node.java
+++ src/LinkListStuff/Node.java
@@ -9,6 +9,10 @@ public class Node<T> {
     public T value;
     public Node next;
 
+    public Node(T value) {
+        this.value = value;
+    }
+
     public Node(T value, Node next) {
         this.value = value;
         this.next = next;
