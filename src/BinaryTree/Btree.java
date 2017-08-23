da5a9f909993ff0000ee9db29f5ed38d061b3e4f
ndw6152 <ndwong19@gmail.com>
2017-08-22 20:41:59 -0700

    Code to check if a binary tree is balance, i.e left sub tree and right subtree do not differ by more than 1 level. Some optimization by doing the check and returning an error value instead of continuously recursing down the tree.

--- src/BinaryTree/Btree.java
+++ src/BinaryTree/Btree.java
@@ -3,6 +3,7 @@ package BinaryTree;
 
 import LinkListStuff.Queue;
 
+import javax.swing.tree.TreeNode;
 import java.util.LinkedList;
 import java.util.Stack;
 
@@ -206,6 +207,27 @@ public class Btree {
 
     }
 
+
+    int checkHeight(Node root) {
+        if (root == null) return -1;
+
+        int leftHeight = checkHeight(root.left);
+        if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Pass error up
+
+        int rightHeight = checkHeight(root.right);
+        if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Pass error up
+
+        int heightDiff = leftHeight - rightHeight;
+        if (Math.abs(heightDiff) > 1) {
+            return Integer.MIN_VALUE; // Found error -> pass it back
+        } else {
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    boolean isBalanced(Node root) {
+        return checkHeight(root) != Integer.MIN_VALUE;
+    }
 
     public static void main(String[] args) {
         Node _25 = new Node(25, null, new Node(45, null, null));
