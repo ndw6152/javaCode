5e27054bf3d05c3ac9207c7c1503ce4c006e5c8e
ndw6152 <ndwong19@gmail.com>
2017-08-22 20:38:27 -0700

    Add numbers stored in linkedlist wihtout the int or long limitation.

--- src/LinkListStuff/Question2_5.java
+++ src/LinkListStuff/Question2_5.java
@@ -7,7 +7,7 @@ public class Question2_5 {
 
     private int mMultiplier = 0;
 
-    public int getIntValue(Node start) {
+    public int getIntvalue(Node start) {
         Node current = start;
         int multiplier = 1;
         int result = 0;
@@ -63,7 +63,7 @@ public class Question2_5 {
 
     }
 
-    public int getIntValueInOder(Node start) {
+    public int getIntvalueInOder(Node start) {
         Node current = start;
         int result = 0;
         int multiplier = 1;
@@ -118,18 +118,92 @@ public class Question2_5 {
 
     }
 
+    // adding two linkedlist correctly that does not take into consideration int limitation
+    class AddTwoNumberClass {
+        Node head = null;
+        Node result = null;
+        int carry = 0;
+
+        public void putNumberInLinkList(int temp) {
+            if(head != null) {
+                if(temp >= 10) {
+                    carry = 1;
+                    Node node = new Node(temp%10);
+                    result.next = node;
+                    result = node;
+                }
+                else {
+                    carry = 0;
+                    Node node = new Node(temp);
+                    result.next = node;
+                    result = node;
+                }
+            }
+            else {
+                if(temp >= 10) {
+                    carry = 1;
+                    Node node = new Node(temp%10);
+                    head = node;
+                    result = node;
+                }
+                else {
+                    carry = 0;
+                    Node node = new Node(temp);
+                    head = node;
+                    result = node;
+                }
+            }
+        }
+
+
+        public Node addTwoNumbers(Node l1, Node l2) {
+
+            while(l1 != null && l2 != null) {
+                int temp = (int) l1.value + + (int) l2.value + carry;
+
+                putNumberInLinkList(temp);
+                l1 = l1.next;
+                l2 = l2.next;
+
+            }
+
+            while(l1 != null) {
+                int temp = (int) l1.value + carry;
+                putNumberInLinkList(temp);
+                l1 = l1.next;
+            }
+
+            while(l2 != null) {
+                int temp = (int) l2.value + carry;
+                putNumberInLinkList(temp);
+                l2 = l2.next;
+            }
+
+            if(carry >0) {
+                if(head != null) {
+                    result.next = new Node(carry);
+                }
+                else {
+                    head = new Node(carry);
+                }
+            }
+            return head;
+
+        }
+    }
+    
     public static void main(String[] args) {
         Question2_5 q = new Question2_5();
 
         Node<Integer> n1 = new Node<>(0, new Node<>(0, new Node<>(9, null)));
         Node<Integer> n2 = new Node<>(0, new Node<>(0, new Node<>(2, null)));
 
-        int val1 = q.getIntValue(n1);
-        int val2 = q.getIntValue(n2);
+        int value1 = q.getIntvalue(n1);
+        int value2 = q.getIntvalue(n2);
 
-        System.out.println(val1+val2);
+        System.out.println(value1+value2);
 
-        Node start = q.createNodeOutOfInt(val1+val2);
+        Node start = q.createNodeOutOfInt(value1+value2);
         Util.printNode(start);
 
 
@@ -137,7 +211,7 @@ public class Question2_5 {
 
         Node<Integer> b1 = new Node<>(9, new Node<>(0, new Node<>(0, null)));
         Node<Integer> b2 = new Node<>(4, new Node<>(0, new Node<>(2, null)));
-        System.out.println(q.getIntValueInOder(b1) + q.getIntValueInOder(b2));
+        System.out.println(q.getIntvalueInOder(b1) + q.getIntvalueInOder(b2));
         Util.printNode(q.createNodeFromInt(912));
 
 
